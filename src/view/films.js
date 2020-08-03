cffa6c771aebc149b732e4e18ffe5b8b4e966678
FINCH64 <meleshko2017@inbox.ru>
2020-08-03 22:44:40 +0300

    Второе задание,модуляция готовы

---
+++ src/view/films.js
@@ -0,0 +1,14 @@
+/* eslint-disable strict */
+export const createFilmListsContainerTemplate = () => {
+  return (
+    `<section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+
+      <div class="films-list__container">
+
+      </div>
+    </section>
+  </section>`
+  );
+};
---
+++ src/view/load-more.js
@@ -0,0 +1,6 @@
+/* eslint-disable strict */
+export const createShowMoreButtonTemplate = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
